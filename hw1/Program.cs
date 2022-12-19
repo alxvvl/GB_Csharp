98b43cd836f6fec813d0e1b09656ecb5639c8a27
Valery Alexeev <valery@MacBook-Pro-Valery.local>
2022-12-19 13:56:16 +0300

    hw1 done

---
+++ hw1/Program.cs
@@ -0,0 +1,95 @@
+﻿// HW 1
+
+// Task 1
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second nubmer: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == num2)
+{
+    Console.Write("equal");    
+}
+else
+{
+    if(num1 > num2)
+    {
+        Console.Write("max = first nubmer");
+    }
+    else
+    {
+        Console.Write("max = second number");
+    }
+}
+*/
+
+//Task 2
+
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    if(num1 > num3)
+    {
+        Console.Write("max = first nubmer");
+    }
+    else
+    {
+        Console.Write("max = third number");
+    }
+}
+else
+{
+    if(num2 > num3)
+    {
+        Console.Write("max = second nubmer");
+    }
+    else
+    {
+        Console.Write("max = third nubmer");
+    }
+}
+*/
+
+//Task 3
+/*
+Console.Write("Input a nubmer: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = num % 2;
+if(current == 0)
+{
+    Console.Write("yes");
+}
+else
+{
+    Console.Write("no");
+}
+*/
+
+//Task 4
+
+/*
+Console.Write("Input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current <= N)
+{
+    while(current % 2 == 0)
+    {
+        Console.Write(current + " ");
+        current++;
+    }
+}
+*/
+
+
+
+
+
